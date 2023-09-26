e22080a20875f815a42c5d62e443f3a497ed3932
Jaime M. Shaker <eyeweld@gmail.com>
2023-09-25 20:54:30 -0500

    Adding Data Mart queires

--- case_studies/case_5_data_mart/data_mart_solutions.sql
+++ case_studies/case_5_data_mart/data_mart_solutions.sql
@@ -121,16 +121,19 @@ week_day  |week_number|month_number|calendar_year|region|platform|segment|age_ba
 
 -- 1. What day of the week is used for each week_date value?
 
-SELECT
-	DISTINCT DATE_PART('dow', week_day)::int AS day_of_week,
-	to_char(week_day, 'Day') AS day_of_week_name
-FROM clean_weekly_sales;
+SELECT DISTINCT 
+	DATE_PART('dow', week_day)::int AS day_of_week,
+	TO_CHAR(week_day, 'Day') AS day_of_week_name
+FROM 
+	clean_weekly_sales;
 
--- Results:
+/*
 
 day_of_week|day_of_week_name|
 -----------+----------------+
           1|Monday          |
+          
+*/          
         
 -- 2. What range of week numbers are missing from the dataset?
 
@@ -140,71 +143,61 @@ WITH RECURSIVE week_count AS (
 	SELECT
 		1 AS week_num
 	UNION ALL
-	SELECT week_num + 1
-	FROM week_count
-	WHERE week_num < 52
+	SELECT 
+		week_num + 1
+	FROM 
+		week_count
+	WHERE 
+		week_num < 52
 )
-
-SELECT week_num AS missing_weeks
-FROM week_count 
-WHERE week_num NOT IN (SELECT DISTINCT week_number FROM clean_weekly_sales);
+SELECT 
+	-- Flatten results
+	STRING_AGG(week_num::TEXT, ',') AS missing_weeks
+FROM 
+	week_count 
+WHERE 
+	week_num NOT IN (
+		SELECT DISTINCT 
+			week_number 
+		FROM 
+			clean_weekly_sales
+	);
 
 -- Or using generate_series function
 
 SELECT
-	*
+	-- Flatten results
+	STRING_AGG(missing_weeks::TEXT, ',') AS missing_weeks
 FROM
-	generate_series(1, 52) AS missing_weeks
-WHERE
-	NOT EXISTS (
+	GENERATE_SERIES(1, 52) AS missing_weeks
+WHERE NOT EXISTS (
 	SELECT
 		1
 	FROM
 		clean_weekly_sales
 	WHERE
-		missing_weeks = week_number);
+		missing_weeks = week_number
+	);
 	
--- Results:
+/*
 	
-missing_weeks|
--------------+
-            1|
-            2|
-            3|
-            4|
-            5|
-            6|
-            7|
-            8|
-            9|
-           10|
-           11|
-           12|
-           37|
-           38|
-           39|
-           40|
-           41|
-           42|
-           43|
-           44|
-           45|
-           46|
-           47|
-           48|
-           49|
-           50|
-           51|
-           52|
+missing_weeks                                                             |
+--------------------------------------------------------------------------+
+1,2,3,4,5,6,7,8,9,10,11,12,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52|
+
+*/
 
 -- 3. How many total transactions were there for each year in the dataset?
 
 SELECT 
 	calendar_year,
 	SUM(transactions) AS total_transactions
-FROM clean_weekly_sales
-GROUP BY calendar_year
-ORDER BY calendar_year;
+FROM 
+	clean_weekly_sales
+GROUP BY 
+	calendar_year
+ORDER BY 
+	calendar_year;
         
 -- Results:
 
@@ -221,14 +214,22 @@ SELECT
 	calendar_year,
 	month_number,
 	SUM(sales) AS total_sales
-FROM clean_weekly_sales
+FROM 
+	clean_weekly_sales
 GROUP BY 
 	region,
 	calendar_year,
 	month_number
-ORDER BY calendar_year, month_number, region;
+ORDER BY 
+	calendar_year, 
+	month_number, 
+	region
+LIMIT 
+	7;
 
--- Results:  Only showing the first month of 2018 of all regions
+/*
+ 
+Limiting results displayed to the first month of 2018 for all regions.
 
 region       |calendar_year|month_number|total_sales|
 -------------+-------------+------------+-----------+
@@ -240,12 +241,15 @@ OCEANIA      |         2018|           3|  175777460|
 SOUTH AMERICA|         2018|           3|   16302144|
 USA          |         2018|           3|   52734998|
 
+*/
+
 -- 5. What is the total count of transactions for each platform?
         
 SELECT 
 	platform,
 	SUM(transactions) AS total_transactions
-FROM clean_weekly_sales
+FROM 
+	clean_weekly_sales
 GROUP BY 
 	platform;      
         
@@ -258,61 +262,70 @@ Retail  |        1081934227|
 
 -- 6. What is the percentage of sales for Retail vs Shopify for each month?
 
+WITH get_total_sales AS (
+	SELECT 
+		platform,
+		calendar_year,
+		month_number,
+		SUM(sales) AS total_sales
+	FROM 
+		clean_weekly_sales
+	GROUP BY 
+		platform,
+		calendar_year,
+		month_number
+	ORDER BY 
+		calendar_year, 
+		month_number, 
+		platform
+)
 SELECT
 	calendar_year,
-	month_number,
+	TO_CHAR(TO_DATE(month_number::TEXT, 'MM'), 'Month') AS month_name,
 	ROUND(100 * SUM(
 		CASE
 			WHEN platform = 'Retail' THEN total_sales
 			ELSE 0
 		END 
-	) / SUM(total_sales), 2) AS retail_perc,
+	) / SUM(total_sales), 2) AS retail_percentage,
 	ROUND(100 * SUM(
 		CASE
 			WHEN platform = 'Shopify' THEN total_sales
 			ELSE 0
 		END 
-	) / SUM(total_sales), 2) AS shopify_perc
-from
-	(SELECT 
-		platform,
-		calendar_year,
-		month_number,
-		SUM(sales) AS total_sales
-	FROM clean_weekly_sales
-	GROUP BY 
-		platform,
-		calendar_year,
-		month_number
-	ORDER BY calendar_year, month_number, platform) AS tmp      
+	) / SUM(total_sales), 2) AS shopify_percentage
+FROM
+	get_total_sales      
 GROUP BY 
 	calendar_year,
 	month_number;
 	
--- Results:
+/*
 	
-calendar_year|month_number|retail_perc|shopify_perc|
--------------+------------+-----------+------------+
-         2018|           3|      97.92|        2.08|
-         2018|           4|      97.93|        2.07|
-         2018|           5|      97.73|        2.27|
-         2018|           6|      97.76|        2.24|
-         2018|           7|      97.75|        2.25|
-         2018|           8|      97.71|        2.29|
-         2018|           9|      97.68|        2.32|
-         2019|           3|      97.71|        2.29|
-         2019|           4|      97.80|        2.20|
-         2019|           5|      97.52|        2.48|
-         2019|           6|      97.42|        2.58|
-         2019|           7|      97.35|        2.65|
-         2019|           8|      97.21|        2.79|
-         2019|           9|      97.09|        2.91|
-         2020|           3|      97.30|        2.70|
-         2020|           4|      96.96|        3.04|
-         2020|           5|      96.71|        3.29|
-         2020|           6|      96.80|        3.20|
-         2020|           7|      96.67|        3.33|
-         2020|           8|      96.51|        3.49|
+calendar_year|month_name|retail_percentage|shopify_percentage|
+-------------+----------+-----------------+------------------+
+         2018|March     |            97.92|              2.08|
+         2018|April     |            97.93|              2.07|
+         2018|May       |            97.73|              2.27|
+         2018|June      |            97.76|              2.24|
+         2018|July      |            97.75|              2.25|
+         2018|August    |            97.71|              2.29|
+         2018|September |            97.68|              2.32|
+         2019|March     |            97.71|              2.29|
+         2019|April     |            97.80|              2.20|
+         2019|May       |            97.52|              2.48|
+         2019|June      |            97.42|              2.58|
+         2019|July      |            97.35|              2.65|
+         2019|August    |            97.21|              2.79|
+         2019|September |            97.09|              2.91|
+         2020|March     |            97.30|              2.70|
+         2020|April     |            96.96|              3.04|
+         2020|May       |            96.71|              3.29|
+         2020|June      |            96.80|              3.20|
+         2020|July      |            96.67|              3.33|
+         2020|August    |            96.51|              3.49|
+         
+*/         
         
 -- 7. What is the percentage of sales by demographic for each year in the dataset? 
 
@@ -320,11 +333,12 @@ SELECT
 	calendar_year,
 	demographics,
 	SUM(sales) AS sales_per_demographic,
-	/* We can nest aggregate functions inside a window function because it operates as a level above the *group by* 
-	 * aime.m.shaker@gmail.com
+	/* We can nest aggregate functions inside a window function because it operates a level above the *group by* 
+	 * jaime.m.shaker@gmail.com
 	 */
-	ROUND(100 * SUM(sales) / SUM(SUM(sales)) OVER (PARTITION BY calendar_year), 2) AS percentage
-FROM clean_weekly_sales
+	ROUND(100 * SUM(sales) / SUM(SUM(sales)) OVER (PARTITION BY calendar_year), 2) AS demographic_percentage
+FROM 
+	clean_weekly_sales
 GROUP BY 
 	demographics,
 	calendar_year
@@ -334,66 +348,68 @@ ORDER BY
 	
 -- Results:
 
-calendar_year|demographics|sales_per_demographic|percentage|
--------------+------------+---------------------+----------+
-         2018|Couples     |           3402388688|     26.38|
-         2018|Families    |           4125558033|     31.99|
-         2018|unknown     |           5369434106|     41.63|
-         2019|Couples     |           3749251935|     27.28|
-         2019|Families    |           4463918344|     32.47|
-         2019|unknown     |           5532862221|     40.25|
-         2020|Couples     |           4049566928|     28.72|
-         2020|Families    |           4614338065|     32.73|
-         2020|unknown     |           5436315907|     38.55|
+calendar_year|demographics|sales_per_demographic|demographic_percentage|
+-------------+------------+---------------------+----------------------+
+         2018|Couples     |           3402388688|                 26.38|
+         2018|Families    |           4125558033|                 31.99|
+         2018|unknown     |           5369434106|                 41.63|
+         2019|Couples     |           3749251935|                 27.28|
+         2019|Families    |           4463918344|                 32.47|
+         2019|unknown     |           5532862221|                 40.25|
+         2020|Couples     |           4049566928|                 28.72|
+         2020|Families    |           4614338065|                 32.73|
+         2020|unknown     |           5436315907|                 38.55|
          
 -- 8.  Which age_band and demographic values contribute the most to Retail sales?
 
-WITH get_total_sales_from_all AS (
-	SELECT
-		demographics,
-		age_band,
-		SUM(sales) AS total_sales,
-		rank() OVER (ORDER BY SUM(sales) desc) AS rnk,
-		ROUND(100 * SUM(sales) / SUM(SUM(sales)) over (), 2) AS percentage
-	FROM 
-		clean_weekly_sales
-	WHERE
-		platform = 'Retail'
-	AND
-		age_band <> 'unknown'
-	GROUP BY 
-		demographics,
-		age_band
-)
 SELECT
-	demographics,
 	age_band,
-	total_sales,
-	percentage
-from
-	get_total_sales_from_all
-WHERE rnk = 1;
+	demographics,
+	SUM(sales) AS total_sales,
+	ROUND(100 * SUM(sales) / SUM(SUM(sales)) OVER ()) AS sales_percentage
+FROM 
+	clean_weekly_sales
+WHERE
+	platform = 'Retail'
+GROUP BY 
+	age_band,
+	demographics
+ORDER BY 
+	sales_percentage DESC;
 
--- Results:
--- ** NOTE ** I did not include 'unknown' because they ask for a demographic and age_band
 
-demographics|age_band|total_sales|percentage|
-------------+--------+-----------+----------+
-Families    |Retirees| 6634686916|     28.13|
+/*
+
+
+age_band    |demographics|total_sales|sales_percentage|
+------------+------------+-----------+----------------+
+unknown     |unknown     |16067285533|              41|
+Retirees    |Families    | 6634686916|              17|
+Retirees    |Couples     | 6370580014|              16|
+Middle Aged |Families    | 4354091554|              11|
+Young Adults|Couples     | 2602922797|               7|
+Middle Aged |Couples     | 1854160330|               5|
+Young Adults|Families    | 1770889293|               4|
+
+*/
 
 -- 9. Can we use the avg_transaction column to find the average transaction size for each year for Retail vs Shopify? 
--- If not - how would you calculate it instead? 
+--    If not - how would you calculate it instead? 
 
 /* 
- * Note: We cannot use the 'average_transactions' column ot calculate avgerage transactions by platform and year
- * as we cannot not get an average of an average and have accurate data.
-**/
+   Note: We cannot use the 'average_transactions' column to find the average transaction size because it is based off of each row!  
+   We cannot calculate 'average_transactions' because we cannot not get an average of an average and have accurate data.
+*/
 
 SELECT
 	calendar_year,
 	platform,
-	(SUM(sales) / SUM(transactions)) AS avg_transaction_size
-FROM clean_weekly_sales
+	-- This is an incorrect way of getting average transaction size.
+	AVG(average_transactions) AS incorrect_avg_transaction_size,
+	-- This is the correct way of getting average transaction size.
+	(SUM(sales) / SUM(transactions)) AS correct_avg_transaction_size
+FROM 
+	clean_weekly_sales
 GROUP BY
 	calendar_year,
 	platform
@@ -401,34 +417,33 @@ ORDER BY
 	calendar_year,
 	platform;
 
--- Results:
+/*
 	
-calendar_year|platform|avg_transaction_size|
--------------+--------+--------------------+
-         2018|Retail  |                  36|
-         2018|Shopify |                 192|
-         2019|Retail  |                  36|
-         2019|Shopify |                 183|
-         2020|Retail  |                  36|
-         2020|Shopify |                 179|
+calendar_year|platform|incorrect_avg_transaction_size|correct_avg_transaction_size|
+-------------+--------+------------------------------+----------------------------+
+         2018|Retail  |           42.4114145658263305|                          36|
+         2018|Shopify |          187.8022519352568614|                         192|
+         2019|Retail  |           41.4719887955182073|                          36|
+         2019|Shopify |          177.0743338008415147|                         183|
+         2020|Retail  |           40.1362044817927171|                          36|
+         2020|Shopify |          174.3964973730297723|                         179|
+         
+*/         
 
 /*
 	Before & After Analysis
 	
-	This technique is usually used when we inspect an important event and want to inspect the 
+	According to Danny, This technique is usually used when we inspect an important event and want to inspect the 
 	impact before and after a certain point in time.
 	
-	Taking the week_date value of 2020-06-15 as the baseline week where the Data Mart sustainable packaging 
-	changes came into effect.
+	Taking the week_date value of 2020-06-15 as the baseline week where the Data Mart sustainable packaging changes came into effect.
 
-	We would include all week_date values for 2020-06-15 as the start of the period after the change and 
-	the previous week_date values would be before
-	
-	Since the week of 2020-06-15 is the base, lets find out what week number this is.
+	We would include all week_date values for 2020-06-15 as the start of the period **after** the change and the 
+	previous week_date values would be **before**.
 */    
          
-SELECT
-	DISTINCT week_number
+SELECT DISTINCT 
+	week_number
 FROM
 	clean_weekly_sales
 WHERE 
@@ -445,37 +460,51 @@ week_number|
 -- rate in actual values and percentage of sales? 
 
 -- 1a. What is the total sales for the 4 weeks before and after 2020-06-15?
-         
+ 
+DROP TABLE IF EXISTS before_after_sales;
+CREATE TEMP TABLE before_after_sales AS (
+	SELECT
+		CASE
+			WHEN week_number BETWEEN 21 AND 24 THEN 'Before'
+			WHEN week_number BETWEEN 25 AND 28 THEN 'After'
+			ELSE null
+		END AS time_period,
+		SUM(sales) AS total_sales,
+		sum(transactions) AS total_transactions,
+		sum(sales) / sum(transactions) AS avg_transaction_size
+	FROM
+		clean_weekly_sales
+	WHERE
+		calendar_year = '2020'
+	AND
+		week_number BETWEEN 21 AND 28
+	GROUP BY 
+		time_period
+	ORDER BY 
+		time_period DESC
+);
+
+WITH get_sales_diff AS (
+	SELECT
+		time_period,
+		total_sales - LAG(total_sales) OVER (ORDER BY time_period) AS sales_difference,
+		round(100 * ((total_sales::NUMERIC / LAG(total_sales) OVER (ORDER BY time_period)) - 1),2) AS sales_change
+	FROM
+		before_after_sales
+)
 SELECT
-	CASE
-		WHEN week_number BETWEEN 21 AND 24 THEN 'Before'
-		WHEN week_number BETWEEN 25 AND 28 THEN 'After'
-		ELSE null
-	END AS time_period,
-	SUM(sales) AS total_sales
+	sales_difference,
+	sales_change
 FROM
-	clean_weekly_sales
+	get_sales_diff
 WHERE
-	calendar_year = '2020'
-GROUP BY 
-	time_period
--- Remove null values from time_period
-HAVING (
-	CASE
-		WHEN week_number BETWEEN 21 AND 24 THEN 'Before'
-		WHEN week_number BETWEEN 25 AND 28 THEN 'After'
-		ELSE null
-	END
-) IS NOT NULL
-ORDER BY 
-	time_period DESC;
+	sales_difference IS NOT NULL;
         
 -- Results:
 	
-time_period|total_sales|
------------+-----------+
-Before     | 2345878357|
-After      | 2318994169|
+sales_difference|sales_change|
+----------------+------------+
+        26884188|        1.16|
 
 -- 1b. What is the growth or reduction rate in actual values and percentage of sales? 
 
